99e6eb848083805e6ac9d771f5a22d161f81eb1c
irvin lin <irvinln@gmail.com>
2019-04-18 20:50:33 +0800

    setup column

--- container-element.js
+++ container-element.js
@@ -1,21 +1,24 @@
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
-    .container {
+    .main-container {
       display: flex;
       height:100vh;
     }
-    .three-quarters {
+    .left-container {
       width: 75%;
       background-color: teal;
+      padding: 30px;
     }
-    .one-quarter {
+    .right-container {
       width: 25%;
       background-color: lightgrey;
     }
   </style>
   <div class="main-container">
-    <div class="left-container"></div>
+    <div class="left-container">
+      <button>Add Column</button>
+    </div>
     <div class="right-container"></div>
   </div>
 `
@@ -28,6 +31,7 @@ class Container extends HTMLElement {
     this.$mainContainer = this._shadowRoot.querySelector('.main-container')
     this.$leftContainer = this._shadowRoot.querySelector('.left-container')
     this.$rightContainer = this._shadowRoot.querySelector('.right-container')
+
   }
 }
 
